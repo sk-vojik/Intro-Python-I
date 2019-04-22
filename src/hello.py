cff98467eca6fc8a141f357ba2068ed27c64c4db
sk-vojik <scott.vojik@gmail.com>
2019-04-22 15:46:41 -0700

    about halfway done, messed up original fork and git flow

--- src/hello.py
+++ src/hello.py
@@ -1,1 +1,19 @@
 # Print "Hello, world!" to your terminal
+print("hello world")
+
+
+# class Animal:
+#   # define attributes, assign initial values
+#   def __init__(self, sound, leg_count=4):
+#     self.leg_count = leg_count
+#     self.sound = sound
+
+#   def set_let_count(self, leg_count):
+#     self.leg_count = leg_count
+
+#   def make_sound(self):
+#     print(self.sound)
+
+# Brooklyn = Animal("bark", 4)
+
+# Brooklyn.make_sound()
